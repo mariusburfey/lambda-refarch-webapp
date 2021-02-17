cbe067d99d8cfeb5800c8ca6aca7a1e5fd21cca6
Marius Burfey <marius.burfey@ambient-innovation.com>
2021-02-17 14:16:35 +0100

    added correct config

--- www/src/config.js
+++ www/src/config.js
@@ -3,13 +3,13 @@
 
 const config = {
   // CognitoClientID:
-  "aws_user_pools_web_client_id": "6rgf4ernuq88k1dud060ce78p5",
+  "aws_user_pools_web_client_id": "522oj2a4vmahns12hqskla3m6",
   // TodoFunctionApi:
-  "api_base_url": "https://nvi9tg8gjc.execute-api.eu-central-1.amazonaws.com/{StageNameParam}",
+  "api_base_url": "https://i2u2t5pqme.execute-api.eu-central-1.amazonaws.com/{StageNameParam}",
   // CognitoDomainName:
   "cognito_hosted_domain": "mytodoappdemo-mb-test-lambda.auth.eu-central-1.amazoncognito.com",
   // AmplifyURL:
-  "redirect_url": "https://master.d1kianoffwvmyw.amplifyapp.com"
+  "redirect_url": "https://master.d2xrfj1xajhtgt.amplifyapp.com"
 };
 
 export default config;
